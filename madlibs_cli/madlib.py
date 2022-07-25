2889b6ac2da3af75dc6bae5a337814bad4627115
Brentice Loper <brentice@icloud.com>
2022-07-25 00:06:31 -0700

    Started to add the write function for the files, but that didn't turn out well.

--- madlibs_cli/madlib.py
+++ madlibs_cli/madlib.py
@@ -7,27 +7,20 @@ print("When you are done, the program will print the completed Madlib.")
 print("********************************************************************************")
 print("                                                                               ")
 
-while True:
-    print("Please enter an adjective:")
-    adjective1 = input("Adjective: ", )
-    print("Please enter an adjective:")
-    adjective2 = input("Adjective: ")
-    print("Please enter a noun:")
-    noun = input("Noun: ")
+print("It is time for Madlib")
+
+def read_template (path):
+    file = open(path, "r")
+    read = file.read()
+    file.close()
+    return read.strip()
+
+print("Please enter an adjective:")
+adjective1 = input("Adjective: ", )
+print("Please enter an adjective:")
+adjective2 = input("Adjective: ")
+print("Please enter a noun:")
+noun = input("Noun: ")
 
 # Read a template Madlib file (Example), and parse that file into usable parts.
 print("It was a " + adjective1 + " and " + adjective2 + " " + noun + ".")
-
-# Prompt the user to continue or quit.
-again = input("Would you like to continue? (y/n) ")
-
-
-def again():
-    if again == "y":
-        again()
-
-    elif again == "n":
-        print("Thank you for using the Madlib program.")
-        print("Goodbye!")
-    else:
-        print("Please enter a valid response.")
