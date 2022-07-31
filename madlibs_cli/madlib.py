fcb77905d796ba57e2353ca615cf479deb87efc9
brentice <brentice@icloud.com>
2022-07-31 12:12:17 -0700

    prints users inputs in sentence.

--- madlibs_cli/madlib.py
+++ madlibs_cli/madlib.py
@@ -2,9 +2,11 @@ import re
 
 print('Welcome to the madlibs generator!')
 print('Please enter a sentence:')
-adjective1 = input('I need an adjective ')
-adjective2 = input('I need an adjective ')
-noun = input('I need a noun ')
+adjective1 = input('Give me an adjective: ')
+adjective2 = input('Give me another adjective: ')
+noun = input('Give me a noun: ')
+
+print("It was a " + adjective1 + " and " + adjective2 + " " + noun + ".")
 
 input_tuple = (adjective1, adjective2, noun)
 
@@ -18,13 +20,16 @@ file.close()
 speech_parts = re.findall(regex, read_template)
 parsed_message = re.sub(regex,'', read_template)
 completedMadlib =  parsed_message.format(adjective1, adjective2, noun)
-print(completedMadlib)
+print(completedMadlib.format)
 
 def merge(b, **a):
   print(a)
   print(b)
   new_sentence = a.format(b)
   return new_sentence
+
+# madlib = merge(read_template, input_tuple)
+# print(madlib)
 
 file = open('assets/dark_and_story_night_template.txt', 'w')
 n = file.write(completedMadlib)
